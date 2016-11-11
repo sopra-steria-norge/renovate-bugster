dbbfa36e38563233908fd9e082a9339249225eb0
emagnus <westerga@gmail.com>
2016-11-11 14:52:44 +0100

    Dårlig test, dårlig assert.

--- bugster-business/src/main/java/no/soprasteria/bugster/business/match/service/MatchService.java
+++ bugster-business/src/main/java/no/soprasteria/bugster/business/match/service/MatchService.java
@@ -1,4 +1,23 @@
 package no.soprasteria.bugster.business.match.service;
 
+import no.soprasteria.bugster.business.match.domain.Match;
+import no.soprasteria.bugster.business.polling.service.ResultService;
+
+import java.util.Collections;
+import java.util.List;
+
 public class MatchService {
+
+    private ResultService resultService = new ResultService();
+
+    public List<Match> findAll() {
+        return resultService.findAll();
+    }
+
+    public List<Match> findSortedMatches() {
+        List<Match> all = findAll();
+        Collections.sort(all);
+        return all;
+    }
+
 }
---
+++ bugster-business/src/test/java/no/soprasteria/bugster/business/match/service/MatchServiceTest.java
@@ -0,0 +1,18 @@
+package no.soprasteria.bugster.business.match.service;
+
+import no.soprasteria.bugster.business.match.domain.Match;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MatchServiceTest {
+
+    @Test
+    public void should_find_all_matches() throws Exception {
+        List<Match> allMatches = new MatchService().findAll();
+
+        assertThat(!allMatches.isEmpty());
+    }
+}
